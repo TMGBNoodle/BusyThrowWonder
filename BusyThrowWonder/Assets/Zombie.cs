8c4ecf7ff365a57e0d23b9c958348b88698ac2d5
TMGBNoodle <tm.grovesbenedict@gmail.com>
2025-03-21 09:22:09 -0500

    Rock added to main scene

--- BusyThrowWonder/Assets/Zombie.cs
+++ BusyThrowWonder/Assets/Zombie.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(PlayerMovement.Instance.transform);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Rock") {
+            Destroy(collision.gameObject); 
+            animator.SetBool("Alive", false);
+            Invoke(Destroy(gameObject), animator.get)
+        }
     }
 }
